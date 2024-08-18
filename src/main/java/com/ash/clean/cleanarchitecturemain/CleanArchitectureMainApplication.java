c75aef65bd6b071cd53fbf509a8db37a610a9c23
hong ash <ash.hong@milkcorp.kr>
2024-08-18 13:29:30 +0900

    first commit

---
+++ src/main/java/com/ash/clean/cleanarchitecturemain/CleanArchitectureMainApplication.java
@@ -0,0 +1,13 @@
+package com.ash.clean.cleanarchitecturemain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CleanArchitectureMainApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CleanArchitectureMainApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ash/clean/cleanarchitecturemain/CleanArchitectureMainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ash.clean.cleanarchitecturemain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CleanArchitectureMainApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
